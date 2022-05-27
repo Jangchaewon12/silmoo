90b82406b4cc1fc6251de0a296fbe04693ef7244
StrongCatWill <86825884+StrongCatWill@users.noreply.github.com>
2022-05-27 18:59:35 +0900

    병합 이전에 세이브본

--- app/src/main/java/com/example/silmoo/yeab_app_main_01.java
+++ app/src/main/java/com/example/silmoo/yeab_app_main_01.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class yeab_app_main_01 extends AppCompatActivity {
 
     Button btn_reservation;
+    Button btn_showmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,15 @@ public class yeab_app_main_01 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(yeab_app_main_01.this, yeab_app_reserve_01.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_showmap = findViewById(R.id.btn_showmap);
+        btn_showmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(yeab_app_main_01.this, yeab_app_mapshow.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/silmoo/yeab_app_mapshow.java
+++ app/src/main/java/com/example/silmoo/yeab_app_mapshow.java
@@ -1,15 +1,40 @@
 package com.example.silmoo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class yeab_app_mapshow extends AppCompatActivity {
+public class yeab_app_mapshow extends AppCompatActivity
+                                implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yeab_app_mapshow);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.yeab_app_mapshow);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+
+        //위도 경도 설정해주기, 처음 보여질 화면임.
+        LatLng School = new LatLng(37.578039079396376, 126.96761698264326 );
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(School);
+        markerOptions.title("배화여자대학교");
+        markerOptions.snippet("학교 주변 1KM 식당가 조회");
+        gMap.addMarker(markerOptions);
+
     }
 }
