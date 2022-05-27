e0386f574422989d5dbabccbf1a528f6a56f4229
StrongCatWill <86825884+StrongCatWill@users.noreply.github.com>
2022-05-27 12:50:24 +0900

    StrongCatWill google Maps api >> yeab_app_mapshow.java로 붙임.

---
+++ app/src/main/java/com/example/silmoo/yeab_app_mapshow.java
@@ -0,0 +1,15 @@
+package com.example.silmoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class yeab_app_mapshow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeab_app_mapshow);
+    }
+}
