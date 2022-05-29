732f53c9544062d70fbea9a1214d4acdc71e3524
JCW <big1201201@gmail.com>
2022-05-30 00:06:07 +0900

    20220529(2)24:05

--- app/src/main/java/com/example/silmoo/yeab_app_main_01.java
+++ app/src/main/java/com/example/silmoo/yeab_app_main_01.java
@@ -1,4 +1,58 @@
+
 package com.example.silmoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class yeab_app_main_01 extends AppCompatActivity {
+
+    Button btn_reservation;
+    Button btn_getCode;
+    Button btn_mapshow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeab_app_main01);
+
+        btn_reservation = findViewById(R.id.btn_reservation);
+        btn_reservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(yeab_app_main_01.this, yeab_app_reserve_01.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_getCode =findViewById(R.id.btn_resfind01);
+        btn_getCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(yeab_app_main_01.this,yeab_app_resfind01.class);
+                startActivity(intent);
+            }
+        });
+
+        /*btn_mapshow= findViewById(R.id.btn_showmap);
+        btn_mapshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(yeab_app_main_01.this,yeab_app_mapshow.class);
+                startActivity(intent);
+            }
+        });*/
+
+
+
+
+    }
+}
+
+/*package com.example.silmoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,4 +82,4 @@ public class yeab_app_main_01 extends AppCompatActivity {
         });
 
     }
-}
+}*/
